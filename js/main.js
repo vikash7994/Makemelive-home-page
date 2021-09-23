5240fab8cd3c9d8d87980445d547b187d99e0f83
vikas gupta <76902305+vikash7994@users.noreply.github.com>
2021-09-23 17:55:48 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$('.banner-slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    fade: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
+ 
+   $('.bottm-slider-main').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: true,
+    fade: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  });
